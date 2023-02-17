68eeee1751261a414e6984e054b359b5edc41d30
Daniel <dlozanoc862@gmail.com>
2023-02-17 12:21:05 -0500

    [ADD] resetForm function

--- js/app.js
+++ js/app.js
@@ -11,12 +11,26 @@ document.addEventListener('DOMContentLoaded', function(){
     const inputAsunto = document.querySelector('#asunto');
     const inputMensaje = document.querySelector('#mensaje');
     const formulario = document.querySelector('#formulario');
-    const btnSubmit = document.querySelector('#formulario button[type="submit"]')
+    const btnSubmit = document.querySelector('#formulario button[type="submit"]');
+    const btnReset = document.querySelector('#formulario button[type="reset"]');
 
     //Listeners
     inputEmail.addEventListener('input', validar);
     inputAsunto.addEventListener('input', validar);
-    inputMensaje.addEventListener('input', validar)
+    inputMensaje.addEventListener('input', validar);
+
+    btnReset.addEventListener('click', resetForm);
+
+    function resetForm(e) {
+        e.preventDefault();
+
+        email.email = '';
+        email.asunto= '';
+        email.mensaje='';
+
+        formulario.reset();
+        comprobarEmail();
+    }
 
     function validar(e){
         if (e.target.value.trim() === '') {

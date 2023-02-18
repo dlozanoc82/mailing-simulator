b6b6a17bf998ea9310cd84e7f5491e6287c6f1ad
Daniel <dlozanoc862@gmail.com>
2023-02-18 11:39:03 -0500

    Form successfully validated

--- js/app.js
+++ js/app.js
@@ -13,13 +13,14 @@ document.addEventListener('DOMContentLoaded', function(){
     const formulario = document.querySelector('#formulario');
     const btnSubmit = document.querySelector('#formulario button[type="submit"]');
     const btnReset = document.querySelector('#formulario button[type="reset"]');
+    const spinner = document.querySelector('#spinner');
 
     //Listeners
     inputEmail.addEventListener('input', validar);
     inputAsunto.addEventListener('input', validar);
     inputMensaje.addEventListener('input', validar);
-
     btnReset.addEventListener('click', resetForm);
+    formulario.addEventListener('submit', enviarEmail);
 
     function resetForm(e) {
         e.preventDefault();
@@ -90,5 +91,32 @@ document.addEventListener('DOMContentLoaded', function(){
             btnSubmit.disabled = false;
         }
     }
-    
+
+    function enviarEmail(e) {
+        e.preventDefault();
+        spinner.classList.add('flex');
+        spinner.classList.remove('hidden');
+
+        setTimeout(() => {
+            spinner.classList.remove('flex');
+            spinner.classList.add('hidden');
+            resetForm(e);
+
+            alertaExito()
+
+        }, 3000);
+
+    }
+
+    function alertaExito() {
+        const alertaExito = document.createElement('p');
+        alertaExito.classList.add('bg-green-500', 'text-white', 'p-2', 'text-center', 'rounded-lg', 'mt-10', 'font-bold', 'text-sm', 'uppercase');
+        alertaExito.textContent = 'Mensaje enviado correctamente !!'
+        formulario.appendChild(alertaExito);
+
+        setTimeout(() => {
+            alertaExito.remove();
+        }, 3000);
+    }
+
 })
